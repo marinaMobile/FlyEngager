a1e773f423ea30da674a8354c384ef6fb2cf5605
Marina <marina@Marinas-MacBook-Pro.local>
2022-12-28 10:33:40 +0300

    all in

--- app/src/main/java/com/superking/parchisi/stara/plc/Brilliant.kt
+++ app/src/main/java/com/superking/parchisi/stara/plc/Brilliant.kt
@@ -3,8 +3,11 @@ package com.superking.parchisi.stara.plc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.superking.parchisi.stara.R
 import com.superking.parchisi.stara.databinding.ActivityBrilliantBinding
+import com.superking.parchisi.stara.one.AviOne
+import com.superking.parchisi.stara.two.AviTwo
 
 class Brilliant : AppCompatActivity() {
     lateinit var bindBril: ActivityBrilliantBinding
@@ -17,6 +20,18 @@ class Brilliant : AppCompatActivity() {
             val intent = Intent(this, PearlActivity::class.java)
             intent.putExtra("WebInt", "policy")
             startActivity(intent)
+        }
+
+        bindBril.one.setOnClickListener {
+            startActivity(Intent(this, AviOne::class.java))
+        }
+
+        bindBril.two.setOnClickListener {
+            startActivity(Intent(this, AviTwo::class.java))
+        }
+
+        bindBril.shop.setOnClickListener {
+            Toast.makeText(this, "SHOP", Toast.LENGTH_SHORT).show()
         }
     }
 }
